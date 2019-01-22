91abc9c60dd1638a3b231dc3d9dd418efc9e23ec
marynovski <kmarynowski@beautifulcompany.pl>
2019-01-22 14:58:18 +0100

    message

--- index.php
+++ index.php
@@ -16,6 +16,8 @@ require __DIR__ . '/vendor/autoload.php';
 include_once 'simple_html_dom.php';
 include_once 'class/GetElement.php';
 
+$companyString = []; //table with companies data
+
 /** @var simple_html_dom $htmlCode */
 $htmlCode = file_get_html("https://infirmy.cz/");
 
@@ -27,53 +29,63 @@ $mainCategoryHref = $element->getItemLoop('h3.hp-categories__item-title > a', $h
 
 /** @var array $count */
 $count = $element->getItemLoop('span.badge.hp-categories__count', $htmlCode, 'innertext');
-$i = 0; //foreach iterator
+$mainCategoryIterator = 0; //foreach iterator
 /**
  * 1. Foreach uses $mainCategoryHref links to get all subCategories links and count of companies
  * 2. Foreach uses $subCategoryHref links to get all companies lists links
  * 3. For loades all pages with company links
  * 4. Foreach gets Company data and saves it to $company[] array
  * 5. Foreach gets $company[] array and do with it whatever I want... now it saves data to .csv
- */
+ */$m = 1;
+ $mainCategoryHrefIterator = 0;
 foreach($mainCategoryHref as $value1){
-    //echo $count[$mainCategoryIterator++].' => '.$value.'<br><hr>';
-
+    //echo $count[$mainCategoryIterator].' => '.$value1.'<br><hr>';
+    $mainCategoryIterator++;
     $htmlCode = file_get_html($value1);
-    /** @var array $subCategoryHref 
+    /** @var array $subCategoryHref
      *  gets all href to subCategories
      */
     $subCategoryHref = $element->getItemLoop('li.categories__item > a', $htmlCode, 'href');
 
 
-    /** @var array $subCount 
+    /** @var array $subCount
      * gets counts of companies in subCategories
      */
     $subCount = $element->getItemLoop('small.categories__count', $htmlCode, 'innertext');
-    
-    $subCountIterator = 0;
-    $iterator = 0;
 
+    $subCountIterator = 0;
+
+
+    if($mainCategoryHrefIterator > 0){
+        break;
+    }
+
+    $subCategoryHrefIterator = 0;
     foreach($subCategoryHref as $value2){
 
-        //echo $subCount[$subCountIterator++].' => '.$value.'<br>';
-        /** delete spaces from count of companies in subCategories */
-        $subCount[$subCountIterator] = str_replace(' ', '', $subCount[$subCountIterator]);
-
-        /**
-         * counting number of pages in one of subCategories
-         */
+        echo $subCount[$subCountIterator++].' => '.$value2.'<br>';
+//        /** delete spaces from count of companies in subCategories */
+//        $subCount[$subCountIterator] = str_replace(' ', '', $subCount[$subCountIterator]);
+//
+//        /**
+//         * counting number of pages in one of subCategories
+//         */
         $pages = ((int)$subCount[$subCountIterator++]/15);
-
+//
+//        /** if $pages is float ++1 */
         if($pages > (int)$pages){
-            $pages++;
+           $pages++;
         }
 
         //echo (int)$pages.'<br>';
 
         $htmlCode = file_get_html($value2);
-        $l = 1;
+//        $l = 1;
         $company = [];
-        //loop looking for all pages
+//        //loop looking for all pages
+        if($subCategoryHrefIterator > 1){
+            break;
+        }
         for($k=1;$k<=(int)$pages;$k++){
             $htmlCode = file_get_html($value2.'?page='.$k);
 
@@ -86,45 +98,54 @@ foreach($mainCategoryHref as $value1){
                 $company['name'] = $element->getItemLoop('h1.firm-detail__title', $htmlCode, 'innertext');
                 $company['address'] = $element->getItemLoop('h2.firm-detail__address', $htmlCode, 'innertext');
 
-                if($l >= 1){
-                    break;
-                }
+                /**stoper */
+//                if($k > 1){
+//                    break;
+//                }
 
 
-                $l++;
+                //$l++;
             }
-            $l = 0;
+            $z = 0;
+
             foreach($company as $value5){
-                //echo 'Firma nr '.$l.'<br>';
-                //echo $company['name'][$l].'<br>';
-                //echo $company['address'][$l].'<br>';
-                //echo '<hr>';
-                $companyString  =   "Firma nr $l"
-                                .   $company['name'][$l]
-                                .   $company['address'][$l];
+                echo 'Firma nr '.$z.'<br>';
+                echo $company['name'][$z].'<br>';
+                echo $company['address'][$z].'<br>';
+                echo '<hr>';
 
+                $companyString[$m-1]  =   "Firma nr $m \n"
+                                .   $company['name'][$z]."\n"
+                                .   $company['address'][$z]."\n\n";
 
-                $file = fopen("firmy.csv", 'a');
-                fwrite($file, $companyString);
-                fclose($file);
+                $m++;
+
 
             }
-            if($k === 1){
+            /**stoper */
+            if($k > 1){
                 break;
             }
         }
+//
+//
+           /**stoper */
 
-
-
-        if($iterator === 5){
-            break;
-        }
-        $iterator++;
+        $subCategoryHrefIterator++;
     }
-    
 
+
+    $mainCategoryHrefIterator++;
 }
 
+/*var_dump($companyString);
+
+
+$file = fopen("firmy.csv", 'a');
+foreach($companyString as $fields){
+    fwrite($file, $fields);
+}
+fclose($file);*/
 
 
 
@@ -135,4 +156,3 @@ foreach($mainCategoryHref as $value1){
 
 
 
-?>
