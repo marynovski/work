dd235020e7a934321ca208d136d4b1c03d29e4b8
marynovski <kmarynowski@beautifulcompany.pl>
2019-03-18 16:08:46 +0100

    grecja

--- grecja.php
+++ grecja.php
@@ -20,7 +20,7 @@ $curl = new Curl();
 
 
 
-//$firmyFile = fopen("paginegialle.csv", 'a');
+$firmyFile = fopen("grecja.csv", 'a');
 //$progresFile = fopen("progres.txt", 'a');
 //$curl->getLastLineInProgressFile()
 for($strona = 1;$strona<=1;$strona++) {
@@ -37,89 +37,129 @@ for($strona = 1;$strona<=1;$strona++) {
     $html2Dom = str_get_html($html2);
     $linki_do_firm = GetElement::getItemLoop('h2.CompanyName > a', $html2Dom, 'href' );
 
-//    print_r($linki_do_firm);
     foreach ($linki_do_firm as $link) {
-//        $link = htmlspecialchars($link);
-//        mb_convert_encoding(urldecode($link),'HTML-ENTITIES','UTF-8');
-        echo $link;
-        $fyrmaHtml = $curl->getHtml($link);
-        echo $fyrmaHtml;
-        die();
-    }
+        $fyrmaHtml = $curl->getHtml("https://www.vrisko.gr/advdetails/6cc44a1_0daj6a3d3k7g3165b___a63f5f463k0cc6_a6a5fag742abebf032c_6?what=%CE%9A%CE%BF%CE%BC%CE%BC%CF%89%CF%84%CE%AE%CF%81%CE%B9%CE%B1%20%CE%9A%CE%BF%CF%85%CF%81%CE%B5%CE%AF%CE%B1&where=&region=1");
+//        echo $fyrmaHtml;
+        $fyrmaHtml = str_get_html($fyrmaHtml);
+        $nazwa = GetElement::getItemLoop('label.companyLabel_class > span', $fyrmaHtml, 'innertext');
+//        print_r($nazwa);
+        $daneAdresowe = GetElement::getItemLoop('td.DetailAddressTd > label', $fyrmaHtml, 'innertext');
+//        print_r($daneAdresowe);
+
+        $daneAdresoweIterator = 0;
+        foreach ($daneAdresowe as $adres) {
+            $rozbiteAdresy = explode(',', $daneAdresowe[$daneAdresoweIterator]);
+            $ulice[$daneAdresoweIterator] = trim($rozbiteAdresy[0]);
+            preg_match('/<meta itemprop\=\"addressLocality\" content\=\"(.*?)\" \/\>/', $daneAdresowe[$daneAdresoweIterator], $miasto);
 
 
-    die();
-
-
-    $nazwy = GetElement::getItemLoop('h2.CompanyName > a > meta', $html2Dom, 'content' );
-    $daneAdresowe = GetElement::getItemLoop('div.AdvAddress', $html2Dom, 'innertext' );
-
-    $daneAdresoweIterator = 0;
-    foreach ($daneAdresowe as $adres) {
-        $rozbiteAdresy = explode(',', $daneAdresowe[$daneAdresoweIterator]);
-        $ulice[$daneAdresoweIterator] = $rozbiteAdresy[0];
-        preg_match('/<meta itemprop\=\"addressLocality\" content\=\"(.*?)\" \/\>/', $daneAdresowe[$daneAdresoweIterator], $miasto);
-        $miasta[$daneAdresoweIterator] = $miasto[1];
+            $miasta[$daneAdresoweIterator] = trim(" Miasto");
 
 //        print_r($rozbiteAdresy);
 
-        foreach ($rozbiteAdresy as $rozbityAdres) {
-            if (preg_match('/^\s[0-9]{5}$/', $rozbityAdres, $kod)) {
-                $kody[$daneAdresoweIterator] = $kod[0];
+            foreach ($rozbiteAdresy as $rozbityAdres) {
+                if (preg_match('/^\s[0-9]{5}$/', $rozbityAdres, $kod)) {
+                    $kody[$daneAdresoweIterator] = trim($kod[0]);
+                }
             }
         }
 
-        $www[$daneAdresoweIterator] = GetElement::getItemLoop('a.siteLink > meta', $html2Dom, 'content');
+        $telefon = GetElement::getBySelector('div.details_list_content_class > label.rc_firstphone', $fyrmaHtml);
+        $www = GetElement::getBySelector('a.rc_Detaillink',$fyrmaHtml);
 
-//        echo $ulice[$daneAdresoweIterator].'<br>';
-//        echo $miasta[$daneAdresoweIterator].'<br>';
-//        echo '<hr>';
+        var_dump($nazwa[0]);
+        var_dump($ulice[0]);
+        var_dump($miasta[0]);
+        var_dump($kody[0]);
+        var_dump($telefon[0]->innertext);
+        var_dump($www[0]->href);
 
 
+        $nazwa = trim($nazwa[0]);
+//        var_dump($nazwa);
+        $adres = trim($ulice[0]);
+//        var_dump($adres);
+        $miasto = trim($miasta[0]);
+//        var_dump($miasto);
+        $kodPocztowy = trim($kody[0]);
+//        var_dump($kodPocztowy);
+        $tel = trim($telefon[0]->innertext);
+//        var_dump($tel);
+        $strona = trim($www[0]->href);
+//        var_dump($strona);
 
-        print_r($nazwy);
-        print_r($ulice);
-        print_r($miasta);
-        print_r($kody);
-        die();
-        if (!empty($nazwa[1])) {
+
+    }
+
+//    $nazwy = GetElement::getItemLoop('h2.CompanyName > a > meta', $html2Dom, 'content' );
+//    $daneAdresowe = GetElement::getItemLoop('div.AdvAddress', $html2Dom, 'innertext' );
+//
+//    $daneAdresoweIterator = 0;
+//    foreach ($daneAdresowe as $adres) {
+//        $rozbiteAdresy = explode(',', $daneAdresowe[$daneAdresoweIterator]);
+//        $ulice[$daneAdresoweIterator] = $rozbiteAdresy[0];
+//        preg_match('/<meta itemprop\=\"addressLocality\" content\=\"(.*?)\" \/\>/', $daneAdresowe[$daneAdresoweIterator], $miasto);
+//        $miasta[$daneAdresoweIterator] = $miasto[1];
+//
+////        print_r($rozbiteAdresy);
+//
+//        foreach ($rozbiteAdresy as $rozbityAdres) {
+//            if (preg_match('/^\s[0-9]{5}$/', $rozbityAdres, $kod)) {
+//                $kody[$daneAdresoweIterator] = $kod[0];
+//            }
+//        }
+//
+//        $www[$daneAdresoweIterator] = GetElement::getItemLoop('a.siteLink > meta', $html2Dom, 'content');
+//
+////        echo $ulice[$daneAdresoweIterator].'<br>';
+////        echo $miasta[$daneAdresoweIterator].'<br>';
+////        echo '<hr>';
+//
+//
+//
+//        print_r($nazwy);
+//        print_r($ulice);
+//        print_r($miasta);
+//        print_r($kody);
+//        die();
+        if (!empty($nazwa)) {
             $companyString = '"';
-            $companyString .= $nazwa[1] . '"' . "\t";
+            $companyString .= $nazwa . '"' . "\t";
         } else {
             $companyString = '"';
             $companyString .= '"' . "\t";
         }
-        if (!empty($adres[1][3])) {
+        if (!empty($adres)) {
             $companyString .= '"';
-            $companyString .= $adres[1][3] . '"' . "\t";
+            $companyString .= $adres . '"' . "\t";
         } else {
             $companyString .= '"';
             $companyString .= '"' . "\t";
         }
-        if (!empty($kodPocztowy[1])) {
+        if (!empty($miasto)) {
             $companyString .= '"';
-            $companyString .= str_replace(' ', '', $kodPocztowy[1]) . '"' . "\t";
+            $companyString .= str_replace(' ', '', $miasto) . '"' . "\t";
         } else {
             $companyString .= '"';
             $companyString .= '"' . "\t";
         }
-        if (!empty($miasto[1])) {
+        if (!empty($kodPocztowy)) {
             $companyString .= '"';
-            $companyString .= $miasto[1] . '"' . "\t";
+            $companyString .= $kodPocztowy . '"' . "\t";
         } else {
             $companyString .= '"';
             $companyString .= '"' . "\t";
         }
-        if (!empty($telefon)) {
+        if (!empty($tel)) {
             $companyString .= '"';
-            $companyString .= $telefon . '"' . "\t";
+            $companyString .= $tel . '"' . "\t";
         } else {
             $companyString .= '"';
             $companyString .= '"' . "\t";
         }
-        if (!empty($www[0])) {
+        if (!empty($strona)) {
             $companyString .= '"';
-            $companyString .= $www[0] . '"' . "\n";
+            $companyString .= $strona . '"' . "\n";
         } else {
             $companyString .= '"';
             $companyString .= '"' . "\n";
@@ -130,8 +170,9 @@ for($strona = 1;$strona<=1;$strona++) {
         echo 'Strona ' . $strona . ' Firma: ' . ($daneAdresoweIterator) . '/20 ' . $companyString . PHP_EOL;
 
         $daneAdresoweIterator++;
+        die();
     }
-}
+
 
 
 fclose($firmyFile);
